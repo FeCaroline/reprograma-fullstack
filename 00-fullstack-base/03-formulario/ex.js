74c307317f840f62e9cfa90d1ab844dfbd19aaea
FeCaroline <39529522+FeCaroline@users.noreply.github.com>
2018-09-19 22:32:50 -0300

    Add files via upload

---
+++ 00-fullstack-base/03-formulario/ex.js
@@ -0,0 +1,44 @@
+// var user = document.getElementById("user").value;
+
+function checkUser() {
+  alert(document.getElementById("user").value);
+
+
+var check=document.getElementById("checkP");
+  check.addEventListener('click', function(){
+
+  });
+
+  var user=document.getElementById("user").value{
+        alert(user);
+  };
+
+
+}
+
+// var password = document.getElementById("password");
+
+// // check.addEventListener('click', function(checkUser) {
+// //   alert(user.value);
+
+
+
+
+
+
+// function checkUser() = document.getElementByClass("user").value{
+//   if ("user" === null) {
+//     alert("Favor inserir usuário");
+//     user.preventDefault();
+//   }
+// }
+
+
+
+
+// function checkPassword() = document.getElementById("password").length{
+//   if ("password" < 6) {
+//     alert("Favor inserir uma senha com mais de 6 caracteres");
+//     password.preventDefault();
+//   }
+
