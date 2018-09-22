75b94644dfa12caac4c28b473c46beda6a84dc34
FeCaroline <39529522+FeCaroline@users.noreply.github.com>
2018-09-21 22:24:24 -0300

    Add files via upload

---
+++ 00-fullstack-base/Exercicio tabuada/app.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+okb.addEventListener('click', function (event) {
+    event.preventDefault();
+    var numberT = document.querySelector("#numberT").value;
+        for (var i = 0; i < 5; i++) {
+        alert(i);
+};
+});
+};
+
